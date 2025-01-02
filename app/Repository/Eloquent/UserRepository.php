4856e28bce2909ddac36ab9a2e7a4c9302de0d53
JoaoRibeiroDeveloper <joaovictor.ribeiro1300@gmail.com>
2025-01-02 14:38:11 +0000

    feat: implement UserRepository

---
+++ app/Repository/Eloquent/UserRepository.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Repository\Eloquent;
+
+use App\Models\User;
+use App\Repository\Contracts\UserRepositoryInterface;
+use App\Repository\Exceptions\NotFoundException;
+
+class UserRepository implements UserRepositoryInterface
+{
+    protected $model;
+
+    public function __construct(User $user)
+    {
+        $this->model = $user;
+    }
+
+    public function findAll(): array
+    {
+        return $this->model->get()->toArray();
+    }
+
+    public function create(array $data): object
+    {
+        return $this->model->create($data);
+    }
+
+    public function update(string $email, array $data): object
+    {
+        $user = $this->find($email);
+        $user->update($data);
+
+        $user->refresh();
+
+        return $user;
+    }
+
+    public function delete(string $email): bool
+    {
+        if (!$user = $this->find($email)) {
+            throw new NotFoundException('User Not Found');
+        }
+
+        return $user->delete();
+    }
+
+    public function find(string $email): ?object
+    {
+        return $this->model->where('email', $email)->first();
+    }
+}
